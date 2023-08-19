650c95ceed30d6dc71b52494a35131913f0124ac
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2023-08-19 18:41:21 +0700

    ✨ feat: adding projectCard & header (portfolio)

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,24 @@
+import ProjectCard from "@/components/portfolio/projectCard";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex flex-col gap-12">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-2xl font-semibold">Things I’ve Explored</h1>
+        <p className="text-sm font-normal">
+          Discover my world of exploration and creativity. This portfolio is a
+          celebration of my passions, adventures, and the joy of discovery.
+        </p>
+      </div>
+      <div className="grid grid-cols-3 gap-y-6">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+    </div>
+  );
+}
---
+++ components/portfolio/projectCard.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import Links from "@/components/icons/Links";
+
+export default function ProjectCard() {
+  return (
+    <Link className="group flex flex-col justify-between gap-3 px-3" href="/portfolio/404">
+      <Image
+        src="https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1115&q=80"
+        alt={""}
+        width={200}
+        height={125}
+        className="rounded-md"
+      />
+      <div>
+        <h1 className="text-base font-semibold">Project Title</h1>
+        <p className="text-sm font-normal">Project Description, Lorem Ipsum</p>
+      </div>
+      <div className="flex gap-2 items-center text-text-primary group-hover:text-primary transition-colors">
+        <Links className="w-4 h-4" />
+        <p className="text-sm font-semibold">app.mgkusumaputra.me</p>
+      </div>
+    </Link>
+  );
+}
