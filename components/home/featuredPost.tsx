810707af54a49afa05e44eabb57d0136b8a2ad4d
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2023-09-30 18:19:37 +0700

    ✨ feat: showing 3 newest blog post

--- components/home/featuredPost.tsx
+++ components/home/featuredPost.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import PostCard from "@/components/home/postCard";
 import Newsletter from "@/components/Newsletter";
+import { formatDate, sortPosts } from "@/lib/posts";
+import { reader } from "@/keystatic/reader";
 
-export default function FeaturedPost() {
+export default async function FeaturedPost() {
+  const posts = sortPosts(await reader.collections.blog.all()).filter((post) =>
+    process.env.NODE_ENV === "production" ? !post.entry.isDraft : true
+  );
+
+  const newestPost = posts.slice(0, 3);
+
   return (
     <div className="flex flex-col">
       <div className="flex flex-col gap-y-4 mb-9">
         <h1 className="text-lg font-semibold">Featured Post</h1>
-        <PostCard />
-        <PostCard />
-        <PostCard />
+        {newestPost.map(({ slug, entry }) => (
+          <PostCard
+            key={slug}
+            href={slug}
+            title={entry.title}
+            date={formatDate(entry.publishedAt)}
+            views={0}
+          />
+        ))}
       </div>
       <Newsletter />
     </div>
--- components/home/postCard.tsx
+++ components/home/postCard.tsx
@@ -1,30 +1,38 @@
 import React from "react";
-import Link from "next/link";
 
 import ArrowUpRight from "@/icons/arrowUpRight";
 import Clock from "@/icons/Clock";
 import Eye from "@/icons/Eye";
+import Link from "next/link";
 
-export default function PostCard() {
+interface postCardProps {
+  href: string
+  title: string;
+  description?: string;
+  date: string;
+  views: number;
+}
+
+export default function PostCard({href, title, description, date, views}: postCardProps) {
   return (
     <Link
-      href={""}
-      className="w-full flex items-center justify-between px-2 py-3 bg-secondary rounded-md"
+      href={`/blog/${href}`}
+      className="w-full flex items-center justify-between px-2 py-3 bg-secondary hover:bg-primary rounded-md transition-colors group"
     >
-      <div className="flex flex-col gap-3">
-        <h2 className="text-base font-bold">Hello World!</h2>
-        <div className="flex flex-row gap-2 text-text-secondary">
+      <div className="flex flex-col gap-3 group-hover:text-background">
+        <h2 className="text-base font-bold">{title}</h2>
+        <div className="flex flex-row gap-2">
           <div className="flex items-center">
             <Clock className="w-4 h-4 mr-1" />
-            <p className="text-xs font-normal">23 Jul, 2023</p>
+            <p className="text-xs font-normal">{date}</p>
           </div>
           <div className="flex items-center">
             <Eye className="w-4 h-4 mr-1" />
-            <p className="text-xs font-normal">23 Jul, 2023</p>
+            <p className="text-xs font-normal">{views}</p>
           </div>
         </div>
       </div>
-      <ArrowUpRight className="text-text-secondary w-6 h-6" />
+      <ArrowUpRight className="text-text-secondary group-hover:text-background w-6 h-6" />
     </Link>
   );
 }
