17371a3fb8ff5af2e68d3835bddc84cb29431ba3
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2024-02-28 21:08:31 +0700

    ✨ feat: updating styling shadcn

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,99 +12,55 @@ export default function Navbar() {
   }
 
   return (
-    <LayoutGroup id="navbar">
-      <nav className="mb-12 text-base font-medium">
-        <ul className="flex items-center gap-x-3">
-          <li>
-            <Link
-              href="/"
-              className={`relative pb-px${
-                pathname === "/"
-                  ? "text-text-primary transition"
-                  : "text-inactive"
-              }`}
-            >
-              Home
-              {pathname === "/" && (
-                <motion.div
-                  transition={{
-                    type: "keyframes",
-                    duration: 0.25,
-                  }}
-                  layoutId="navbar"
-                  className="absolute inset-x-0 h-px bottom-0 bg-gradient-to-l from-gray-200 via-gray-400 to-gray-600"
-                ></motion.div>
-              )}
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/portfolio"
-              className={`relative pb-px${
-                pathname === "/portfolio"
-                  ? "text-text-primary transition"
-                  : "text-inactive"
-              }`}
-            >
-              Portfolio
-              {pathname === "/portfolio" && (
-                <motion.div
-                  transition={{
-                    type: "keyframes",
-                    duration: 0.25,
-                  }}
-                  layoutId="navbar"
-                  className="absolute inset-x-0 h-px bottom-0 bg-gradient-to-l from-gray-200 via-gray-400 to-gray-600"
-                ></motion.div>
-              )}
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/blog"
-              className={`relative pb-px${
-                pathname === "/blog"
-                  ? "text-text-primary transition"
-                  : "text-inactive"
-              }`}
-            >
-              Blog
-              {pathname === "/blog" && (
-                <motion.div
-                  transition={{
-                    type: "keyframes",
-                    duration: 0.25,
-                  }}
-                  layoutId="navbar"
-                  className="absolute inset-x-0 h-px bottom-0 bg-gradient-to-l from-gray-200 via-gray-400 to-gray-600"
-                ></motion.div>
-              )}
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/guestbook"
-              className={`relative pb-px${
-                pathname === "/guestbook"
-                  ? "text-text-primary transition"
-                  : "text-inactive"
-              }`}
-            >
-              Guestbook
-              {pathname === "/guestbook" && (
-                <motion.div
-                  transition={{
-                    type: "keyframes",
-                    duration: 0.25,
-                  }}
-                  layoutId="navbar"
-                  className="absolute inset-x-0 h-px bottom-0 bg-gradient-to-l from-gray-200 via-gray-400 to-gray-600"
-                ></motion.div>
-              )}
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </LayoutGroup>
+    <nav className="mb-12 text-base font-medium">
+      <ul className="flex items-center gap-x-3">
+        <li>
+          <Link
+            href="/"
+            className={`relative pb-px${
+              pathname === "/" ? "text-primary- transition" : "text-inactive"
+            }`}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/portfolio"
+            className={`relative pb-px${
+              pathname === "/portfolio"
+                ? "text-text-primary transition"
+                : "text-inactive"
+            }`}
+          >
+            Portfolio
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/blog"
+            className={`relative pb-px${
+              pathname === "/blog"
+                ? "text-text-primary transition"
+                : "text-inactive"
+            }`}
+          >
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/guestbook"
+            className={`relative pb-px${
+              pathname === "/guestbook"
+                ? "text-text-primary transition"
+                : "text-inactive"
+            }`}
+          >
+            Guestbook
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
