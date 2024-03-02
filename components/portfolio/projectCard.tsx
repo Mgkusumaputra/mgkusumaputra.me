89a88857b68cb2a271b6a70ddf721852c128956a
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2024-03-02 17:47:33 +0700

    💎 style(project): fixing project card styling

--- components/portfolio/projectCard.tsx
+++ components/portfolio/projectCard.tsx
@@ -20,7 +20,7 @@ export default function ProjectCard({
 }: projectCardProps) {
   return (
     <Link
-      className="group w-full sm:w-auto flex flex-col justify-between gap-3 px-3 py-6 rounded-md transition-colors hover:bg-secondary"
+      className="group w-full sm:w-auto flex flex-col gap-3 px-3 py-6 rounded-md transition-colors hover:bg-secondary"
       href={redirect}
       target="_blank"
     >
@@ -31,17 +31,19 @@ export default function ProjectCard({
         height={125}
         className="rounded-md"
       />
-      <div className="flex flex-col gap-2">
-        <h1 className="text-base font-semibold">{title}</h1>
-        <p className="text-sm font-normal dark:text-muted-foreground">
-          {description}
-        </p>
-      </div>
-      <div className="flex gap-2 items-center text-secondary-foreground group-hover:text-primary transition-colors">
-        <LinkIcon className="w-4 h-4" />
-        <p className="text-sm font-semibold group-hover:underline">
-          {projectURL}
-        </p>
+      <div className="flex flex-col h-full justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-base font-semibold">{title}</h1>
+          <p className="text-sm font-normal dark:text-muted-foreground">
+            {description}
+          </p>
+        </div>
+        <div className="flex gap-2 items-center mt-2 text-secondary-foreground group-hover:text-primary transition-colors">
+          <LinkIcon className="w-4 h-4" />
+          <p className="text-sm font-semibold group-hover:underline">
+            {projectURL}
+          </p>
+        </div>
       </div>
     </Link>
   );
