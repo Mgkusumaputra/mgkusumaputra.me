7869909e16b6324cf9f54f7c93fa081f851a52e5
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2024-03-02 10:38:04 +0700

    ✨ feat(certificate): change certificate card styling

--- components/certificate/certificateCard.tsx
+++ components/certificate/certificateCard.tsx
@@ -9,21 +9,31 @@ interface CertificateCardProps {
   issueDate: string;
 }
 
-export default function CertificateCard({href="/", imageLogo, name, org, issueDate=""}: CertificateCardProps) {
+export default function CertificateCard({
+  href = "/",
+  imageLogo,
+  name,
+  org,
+  issueDate = "",
+}: CertificateCardProps) {
   return (
     <Link href={href} className="group">
-      <div className="flex items-center gap-6 px-2 py-2 bg-secondary rounded transition-colors group-hover:bg-primary group-hover:text-background">
+      <div className="flex items-center gap-6 px-2 py-2 bg-secondary text-muted-foreground rounded transition-colors group-hover:bg-primary group-hover:text-secondary dark:group-hover:text-secondary-foreground">
         <Image
           src={imageLogo}
           alt=""
           width={40}
           height={40}
-          className="rounded w-10 h-10"
+          className="rounded w-10 h-10 filter grayscale group-hover:filter-none group-hover:transition-opacity"
         />
         <div>
           <h1 className="text-base font-bold">{name}</h1>
 
-          <p className="text-xs font-normal">{org} • {issueDate}</p>
+          <div className="flex gap-1">
+            <p className="text-xs font-normal">{org}</p>
+            <p className="text-xs font-normal">•</p>
+            <p className="text-xs font-normal">{issueDate}</p>
+          </div>
         </div>
       </div>
     </Link>
