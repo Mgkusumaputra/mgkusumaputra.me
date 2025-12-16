8e6b0abaa5388028e3651c5a3db88722ba7dec0b
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2025-12-17 05:42:46 +0700

    feat: remove unnecessary comment

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,9 @@
 "use client";
-import { useCallback } from "react";
 import { ThemeToggleButton } from "./themeToggle";
 import { useTheme } from "next-themes";
 
 export default function Navbar() {
-  const { setTheme, resolvedTheme } = useTheme();
-  // const { startTransition } = useThemeTransition();
-
-  // const isDark = resolvedTheme === "dark";
-
-  // const handleThemeToggle = useCallback(() => {
-  //   startTransition(() => {
-  //     setTheme(isDark ? "light" : "dark");
-  //   });
-  // }, [setTheme, isDark, startTransition]);
+  const { resolvedTheme } = useTheme();
   return (
     <nav className="p-6 flex justify-end">
       <ThemeToggleButton theme={resolvedTheme as "light" | "dark"} />
