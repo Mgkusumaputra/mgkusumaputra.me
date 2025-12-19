9461a44674daa42f01c34155251ed923ef245ee6
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2025-12-19 21:23:26 +0700

    feat: add backward navigation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,35 @@
 "use client";
+
+import Link from "next/link";
+import { ArrowLeftIcon } from "./icons/arrowLeftIcon";
 import { ThemeToggleButton } from "./themeToggle";
 import { useTheme } from "next-themes";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const { resolvedTheme } = useTheme();
+  const pathname = usePathname();
+
   return (
-    <nav className="p-6 flex justify-end">
+    <nav className="p-6 flex justify-between items-center">
+      <div className="flex items-center gap-3">
+        {!pathname.startsWith("/writing/") && (
+          <Link href="/" aria-label="Go home" className="cursor-pointer">
+            <ArrowLeftIcon size={20} />
+          </Link>
+        )}
+
+        {pathname.startsWith("/writing/") && (
+          <Link
+            href="/writing"
+            aria-label="Back to writing"
+            className="cursor-pointer text-sm text-secondary hover:opacity-75"
+          >
+            cd ../writing
+          </Link>
+        )}
+      </div>
+
       <ThemeToggleButton theme={resolvedTheme as "light" | "dark"} />
     </nav>
   );
