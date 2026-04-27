60b53d49efdc5fc80bff8ecca5d7d369404cf0ad
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2026-04-27 21:36:42 +0700

    fix: writing date sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,21 +1,17 @@
-import { getAllWritingStatics } from '@/lib/writing';
-import type { MetadataRoute } from 'next'
+import { FormatDate } from "@/lib/formatDate";
+import { getAllWritingStatics } from "@/lib/writing";
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const all_writings = getAllWritingStatics().map((writing) => ({
-        url: `https://mgkusumaputra.me/writing/${writing.slug}`,
-        lastModified: new Date(writing.date).toISOString().split("T")[0],
-    }))
+  const all_writings = getAllWritingStatics().map((writing) => ({
+    url: `https://mgkusumaputra.me/writing/${writing.slug}`,
+    lastModified: new Date(FormatDate(writing.date)).toISOString().split("T")[0],
+  }));
 
-    const all_routes = [
-        "",
-        "/projects",
-        "/writing",
-        "/about-me",
-    ].map((route) => ({
-        url: `https://mgkusumaputra.me${route}`,
-        lastModified: new Date().toISOString().split("T")[0],
-    }));
+  const all_routes = ["", "/projects", "/writing", "/about-me"].map((route) => ({
+    url: `https://mgkusumaputra.me${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
 
-    return [...all_routes, ...all_writings];
+  return [...all_routes, ...all_writings];
 }
