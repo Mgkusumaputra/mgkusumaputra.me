6cd2d33467757981b08d2b0e61f7ccdedc94ebdb
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2026-04-28 07:22:37 +0700

    fix: blog og image not working

--- app/writing/[slug]/opengraph-image.tsx
+++ app/writing/[slug]/opengraph-image.tsx
@@ -1,6 +1,8 @@
 import { ImageResponse } from "next/og";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 export const size = {
   width: 1200,
@@ -10,21 +12,16 @@ export const size = {
 export const contentType = "image/png";
 
 async function getWritingBySlug(slug: string) {
-  const writings = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/writings.json`,
-    { cache: "force-cache" }
-  ).then((res) => res.json());
+  const writings = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/writings.json`, { cache: "force-cache" }).then((res) =>
+    res.json(),
+  );
 
   const writing = await writings.find((w: { slug: string }) => w.slug === slug);
 
   return writing;
 }
 
-export default async function Image({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
+export default async function Image({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
 
   const writing = await getWritingBySlug(slug);
@@ -33,50 +30,44 @@ export default async function Image({
     return new Response("Not Found", { status: 404 });
   }
 
-  const font = fetch(
-    new URL("../../../public/fonts/PlusJakartaSans-Medium.ttf", import.meta.url)
-  ).then((res) => res.arrayBuffer());
-  const fontData = await font;
+  const PJSData = await readFile(join(process.cwd(), "/public/fonts/PlusJakartaSans-Medium.ttf"));
 
   return new ImageResponse(
-    (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "center",
+        backgroundImage: `url(${process.env.OG_BACKGROUND_URL})`,
+      }}
+    >
       <div
         style={{
-          height: "100%",
-          width: "100%",
+          width: 760,
+          marginLeft: 80,
           display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          justifyContent: "center",
-          backgroundImage: `url(${process.env.OG_BACKGROUND_URL})`,
+          fontSize: 64,
+          color: "#0E0E0D",
+          lineHeight: "80px",
+          letterSpacing: "0px",
+          whiteSpace: "pre-wrap",
         }}
       >
-        <div
-          style={{
-            width: 760,
-            marginLeft: 80,
-            display: "flex",
-            fontSize: 64,
-            color: "#0E0E0D",
-            lineHeight: "80px",
-            letterSpacing: "0px",
-            whiteSpace: "pre-wrap",
-          }}
-        >
-          {writing.title}
-        </div>
+        {writing.title}
       </div>
-    ),
+    </div>,
     {
       ...size,
       fonts: [
         {
           name: "Plus Jakarta Sans",
-          data: fontData,
+          data: PJSData,
           style: "normal",
         },
       ],
-    }
+    },
   );
 }
-
